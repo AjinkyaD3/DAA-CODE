949708e6f7723dd131f553474f8c9c0ca1fedc7c
Ajinkya D <138004402+AjinkyaD3@users.noreply.github.com>
2025-11-11 22:50:23 +0530

    Refactor Node structure and main function
    Refactor Node structure and improve boundCalc function for clarity. Update main function to enhance user interaction and streamline the algorithm.

--- exp4.cpp
+++ exp4.cpp
@@ -10,59 +10,96 @@ and the cost using LC Branch and Bound.
 #include <queue>
 using namespace std;
 
-struct Node{
+struct Node {
     vector<int> path;
     vector<bool> vis;
     int cost, lb, lvl;
-    bool operator>(const Node& o) const { return lb > o.lb; }
+
+    bool operator>(const Node& o) const {
+        return lb > o.lb; // For min-priority queue (lower bound first)
+    }
 };
 
-int boundCalc(const Node& nd, vector<vector<int>>& d, int n){
-    int b = nd.cost, last = nd.path.back();
-    for(int i=0;i<n;i++){
-        if(!nd.vis[i]){
+// Calculate Lower Bound
+int boundCalc(const Node& nd, vector<vector<int>>& d, int n) {
+    int b = nd.cost;
+    for(int i = 0; i < n; i++) {
+        if(!nd.vis[i]) {
             int mn = INT_MAX;
-            for(int j=0;j<n;j++) if(i!=j) mn = min(mn, d[j][i]);
+            for(int j = 0; j < n; j++) {
+                if(i != j) mn = min(mn, d[j][i]);
+            }
             b += mn;
         }
     }
     return b;
 }
 
-int main(){
-    int n; cin>>n;
+int main() {
+    int n;
+
+    cout << "Enter number of cities: ";
+    cin >> n;
+
     vector<vector<int>> d(n, vector<int>(n));
-    for(int i=0;i<n;i++) for(int j=0;j<n;j++) cin>>d[i][j];
 
-    priority_queue<Node,vector<Node>,greater<Node>> pq;
-    Node st; 
-    st.path={0}; st.vis=vector<bool>(n,false); st.vis[0]=true;
-    st.cost=0; st.lvl=1; st.lb = boundCalc(st,d,n);
-    pq.push(st);
+    cout << "\nEnter cost matrix (Distance between cities):\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> d[i][j];
+        }
+    }
 
-    int best = INT_MAX; vector<int> bestPath;
+    priority_queue<Node, vector<Node>, greater<Node>> pq;
 
-    while(!pq.empty()){
-        Node cur = pq.top(); pq.pop();
-        if(cur.lb >= best) continue;
-        if(cur.lvl == n){
-            int total = cur.cost + d[cur.path.back()][0];
-            if(total < best){ best = total; bestPath = cur.path; bestPath.push_back(0); }
+    Node start;
+    start.path = {0};                 // Start at city 0
+    start.vis = vector<bool>(n, false);
+    start.vis[0] = true;
+    start.cost = 0;
+    start.lvl = 1;
+    start.lb = boundCalc(start, d, n);
+    pq.push(start);
+
+    int best = INT_MAX;
+    vector<int> bestPath;
+
+    while(!pq.empty()) {
+        Node cur = pq.top();
+        pq.pop();
+
+        if(cur.lb >= best)
+            continue;
+
+        if(cur.lvl == n) {
+            int tourCost = cur.cost + d[cur.path.back()][0];
+            if(tourCost < best) {
+                best = tourCost;
+                bestPath = cur.path;
+                bestPath.push_back(0); 
+            }
             continue;
         }
-        for(int i=0;i<n;i++){
-            if(!cur.vis[i]){
+
+        for(int i = 0; i < n; i++) {
+            if(!cur.vis[i]) {
                 Node nxt = cur;
                 nxt.path.push_back(i);
-                nxt.vis[i]=true;
+                nxt.vis[i] = true;
                 nxt.cost += d[cur.path.back()][i];
                 nxt.lvl++;
-                nxt.lb = boundCalc(nxt,d,n);
-                if(nxt.lb < best) pq.push(nxt);
+                nxt.lb = boundCalc(nxt, d, n);
+
+                if(nxt.lb < best)
+                    pq.push(nxt);
             }
         }
     }
 
-    cout<<"Minimum Cost: "<<best<<"\nPath: ";
-    for(int i: bestPath) cout<<i<<" ";
+    cout << "\nMinimum Cost: " << best << endl;
+    cout << "Path: ";
+    for(int city : bestPath) cout << city << " ";
+
+    cout << endl;
+    return 0;
 }
